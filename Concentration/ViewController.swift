611f5add58bf14ab14be1dbe6f221187c9abe6f7
Tofique Ahmed Khan <tofiqueahmedkhan17@gmail.com>
2018-04-23 13:31:21 +0530

    UI and its connection are Completed

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet weak var cardButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func touchButton(_ sender: UIButton) {
+        
     }
-
+    
 
 }
 
